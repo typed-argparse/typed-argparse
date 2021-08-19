5ba532a1e78573e3a5c0e0855b4dc5403ee16650
Fabian Keller <github.100.fkeller@spamgourmet.com>
2021-08-19 20:37:24 +0200

    added handling of extra fields

--- tests/test_typed_argparse.py
+++ tests/test_typed_argparse.py
@@ -13,20 +13,50 @@ def test_basic() -> None:
     assert args.foo == "foo"
 
 
-def test_field_missing__single() -> None:
+def test_missing_field__single() -> None:
     class MyArgs(TypedArgs):
         foo: str
 
     args_namespace = argparse.Namespace()
-    with pytest.raises(TypeError, match="Arguments object is missing attribute 'foo'"):
+    with pytest.raises(
+        TypeError,
+        match="Arguments object is missing attribute 'foo'",
+    ):
         MyArgs(args_namespace)
 
 
-def test_field_missing__multiple() -> None:
+def test_missing_field__multiple() -> None:
     class MyArgs(TypedArgs):
         foo: str
         bar: str
 
     args_namespace = argparse.Namespace()
-    with pytest.raises(TypeError, match=r"Arguments object is missing attributes \['foo', 'bar'\]"):
+    with pytest.raises(
+        TypeError,
+        match=r"Arguments object is missing attributes \['foo', 'bar'\]",
+    ):
+        MyArgs(args_namespace)
+
+
+def test_extra_field__single() -> None:
+    class MyArgs(TypedArgs):
+        foo: str
+
+    args_namespace = argparse.Namespace(foo="foo", bar="bar")
+    with pytest.raises(
+        TypeError,
+        match="Arguments object has an unexpected extra attribute 'bar'",
+    ):
+        MyArgs(args_namespace)
+
+
+def test_extra_field__multiple() -> None:
+    class MyArgs(TypedArgs):
+        foo: str
+
+    args_namespace = argparse.Namespace(foo="foo", bar="bar", baz="baz")
+    with pytest.raises(
+        TypeError,
+        match=r"Arguments object has an unexpected extra attributes \['bar', 'baz'\]",
+    ):
         MyArgs(args_namespace)
--- typed_argparse/typed_argparse.py
+++ typed_argparse/typed_argparse.py
@@ -10,14 +10,26 @@ class TypedArgs:
         missing_fields: List[str] = []
 
         for name, annotation in self.__annotations__.items():
-
             if not hasattr(args, name):
                 missing_fields.append(name)
             else:
                 self.__dict__[name] = getattr(args, name)
 
+        # Handle missing fields
         if len(missing_fields) > 0:
             if len(missing_fields) == 1:
                 raise TypeError(f"Arguments object is missing attribute '{missing_fields[0]}'")
             else:
                 raise TypeError(f"Arguments object is missing attributes {missing_fields}")
+
+        # Handle extra fields
+        extra_fields = list(set(args.__dict__.keys()) - set(self.__annotations__.keys()))
+        if len(extra_fields) > 0:
+            if len(extra_fields) == 1:
+                raise TypeError(
+                    f"Arguments object has an unexpected extra attribute '{extra_fields[0]}'"
+                )
+            else:
+                raise TypeError(
+                    f"Arguments object has an unexpected extra attributes {extra_fields}"
+                )
