0e8ba0807cc5a53c20cb736c0502778ed7db4042
Fabian Keller <github.100.fkeller@spamgourmet.com>
2021-08-20 21:02:07 +0200

    better naming conventions

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 💡 type-safe args for argparse without much refactoring.
 
 
-[![PyPI version](https://badge.fury.io/py/typed_argparse.svg)](https://badge.fury.io/py/typed_argparse)
+[![PyPI version](https://badge.fury.io/py/typed-argparse.svg)](https://badge.fury.io/py/typed_argparse)
 [![Build Status](https://github.com/bluenote10/typed_argparse/workflows/ci/badge.svg)](https://github.com/bluenote10/typed_argparse/actions?query=workflow%3Aci)
 [![codecov](https://codecov.io/gh/bluenote10/typed_argparse/branch/master/graph/badge.svg?token=6I98R2661Z)](https://codecov.io/gh/bluenote10/typed_argparse)
 [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
--- typed_argparse/typed_argparse.py
+++ typed_argparse/typed_argparse.py
@@ -13,9 +13,9 @@ class TypedArgs:
 
         missing_fields: List[str] = []
 
-        for name, attribute_type in self.__annotations__.items():
+        for name, argument_type in self.__annotations__.items():
             if name == "get_raw_args":
-                raise TypeError("A type must not have an attribute called 'get_raw_args'")
+                raise TypeError("A type must not have an argument called 'get_raw_args'")
 
             if not hasattr(args, name):
                 missing_fields.append(name)
@@ -25,39 +25,39 @@ class TypedArgs:
                 underlying_type = None
                 is_optional = False
 
-                if type_utils.is_list(attribute_type):
-                    underlying_type = type_utils.get_underlying_type_of_list(attribute_type)
-                    attribute_type = list
+                if type_utils.is_list(argument_type):
+                    underlying_type = type_utils.get_underlying_type_of_list(argument_type)
+                    argument_type = list
 
-                if hasattr(attribute_type, "__origin__"):
-                    if attribute_type.__origin__ is Union and len(attribute_type.__args__) == 2:
-                        if attribute_type.__args__[0] == _NoneType:
+                if hasattr(argument_type, "__origin__"):
+                    if argument_type.__origin__ is Union and len(argument_type.__args__) == 2:
+                        if argument_type.__args__[0] == _NoneType:
                             is_optional = True
-                            attribute_type = attribute_type.__args__[1]
-                        elif attribute_type.__args__[1] == _NoneType:
+                            argument_type = argument_type.__args__[1]
+                        elif argument_type.__args__[1] == _NoneType:
                             is_optional = True
-                            attribute_type = attribute_type.__args__[0]
+                            argument_type = argument_type.__args__[0]
 
                 if is_optional:
-                    if not isinstance(x, attribute_type) and not (x is None):
+                    if not isinstance(x, argument_type) and not (x is None):
                         raise TypeError(
-                            f"Type of attribute '{name}' should be "
-                            f"Optional[{attribute_type.__name__}], but is "
+                            f"Type of argument '{name}' should be "
+                            f"Optional[{argument_type.__name__}], but is "
                             f"{type(x).__name__}"
                         )
 
                 else:
-                    if not isinstance(x, attribute_type):
+                    if not isinstance(x, argument_type):
                         raise TypeError(
-                            f"Type of attribute '{name}' should be "
-                            f"{attribute_type.__name__}, but is "
+                            f"Type of argument '{name}' should be "
+                            f"{argument_type.__name__}, but is "
                             f"{type(x).__name__}"
                         )
 
                 if underlying_type is not None and hasattr(x, "__iter__"):
                     if not all(isinstance(element, underlying_type) for element in x):
                         raise TypeError(
-                            f"Not all elements of attribute '{name}' are of type "
+                            f"Not all elements of argument '{name}' are of type "
                             f"{underlying_type.__name__}"
                         )
 
@@ -66,20 +66,20 @@ class TypedArgs:
         # Handle missing fields
         if len(missing_fields) > 0:
             if len(missing_fields) == 1:
-                raise TypeError(f"Arguments object is missing attribute '{missing_fields[0]}'")
+                raise TypeError(f"Arguments object is missing argument '{missing_fields[0]}'")
             else:
-                raise TypeError(f"Arguments object is missing attributes {missing_fields}")
+                raise TypeError(f"Arguments object is missing arguments {missing_fields}")
 
         # Handle extra fields
         extra_fields = sorted(set(args.__dict__.keys()) - set(self.__annotations__.keys()))
         if len(extra_fields) > 0:
             if len(extra_fields) == 1:
                 raise TypeError(
-                    f"Arguments object has an unexpected extra attribute '{extra_fields[0]}'"
+                    f"Arguments object has an unexpected extra argument '{extra_fields[0]}'"
                 )
             else:
                 raise TypeError(
-                    f"Arguments object has an unexpected extra attributes {extra_fields}"
+                    f"Arguments object has an unexpected extra arguments {extra_fields}"
                 )
 
     def get_raw_args(self) -> argparse.Namespace:
